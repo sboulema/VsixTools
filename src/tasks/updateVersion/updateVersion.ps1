4f724950c3f2bb5bca518c3ed16adbb13944e69d
Samir L. Boulema <sboulema@gmail.com>
2018-09-17 16:24:06 +0200

    Add UploadVsix task

--- src/tasks/updateVersion/updateVersion.ps1
+++ src/tasks/updateVersion/updateVersion.ps1
@@ -1,3 +1,15 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$True)]
+    [String]$FileName,
+
+    [Parameter(Mandatory=$True)]
+    [string]$VersionNumber
+)
+
+# Set a flag to force verbose as a default
+$VerbosePreference = 'Continue' # equiv to -verbose
+
 function Vsix-IncrementVsixVersion {
     [cmdletbinding()]
     param (
@@ -35,3 +47,11 @@ function Vsix-IncrementVsixVersion {
         }
     }
 }
+
+Function Main() {
+
+    Vsix-IncrementVsixVersion $FileName $VersionNumber
+
+}
+
+Main
