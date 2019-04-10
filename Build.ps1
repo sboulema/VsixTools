b04c78c7ec464febd61bc2e672dac39624acbaef
Samir L. Boulema <samir@auto.nl>
2019-04-10 09:20:25 +0200

    Fix error in Build script, install ps_modules to correct subfolder #8

--- Build.ps1
+++ Build.ps1
@@ -6,4 +6,6 @@ Save-Module -Name VstsTaskSdk -Path .\src\tasks\signVsix\ps_modules
 Move-Item -Path .\src\tasks\signVsix\ps_modules\VstsTaskSdk\0.11.0\* -Destination .\src\tasks\signVsix\ps_modules\VstsTaskSdk
 
 # Copy VstsTaskSdk to uploadVsix
-Copy-Item .\src\tasks\signVsix\ps_modules\* -Destination .\src\tasks\uploadVsix\ps_modules -Recurse
+New-Item -ItemType directory -Path .\src\tasks\uploadVsix\ps_modules
+Save-Module -Name VstsTaskSdk -Path .\src\tasks\uploadVsix\ps_modules
+Move-Item -Path .\src\tasks\uploadVsix\ps_modules\VstsTaskSdk\0.11.0\* -Destination .\src\tasks\uploadVsix\ps_modules\VstsTaskSdk
