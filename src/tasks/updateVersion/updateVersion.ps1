c51a58696961281a4b320b42f1a1893ef924dcc2
Samir L. Boulema <samir@auto.nl>
2019-12-16 16:42:59 +0100

    Using deprecated task execution handler (#18)

--- src/tasks/updateVersion/updateVersion.ps1
+++ src/tasks/updateVersion/updateVersion.ps1
@@ -1,56 +1,44 @@
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$True)]
-    [String]$FileName,
-
-    [Parameter(Mandatory=$True)]
-    [string]$VersionNumber
-)
-
-# Set a flag to force verbose as a default
-$VerbosePreference = 'Continue' # equiv to -verbose
-
-function Vsix-IncrementVsixVersion {
-    [cmdletbinding()]
-    param (
-        [Parameter(Position=0, Mandatory=0)]
-        [string[]]$manifestFilePath = ".\source.extension.vsixmanifest",
-
-        [Parameter(Position=1, Mandatory=0)]
-        [string]$buildNumber = $env:BUILD_BUILDNUMBER
-    )
-    process {
-        foreach($manifestFile in $manifestFilePath)
-        {
-            "Incrementing VSIX version..." | Write-Host  -ForegroundColor Cyan -NoNewline
-            $matches = (Get-ChildItem $manifestFile -Recurse)
-            $vsixManifest = $matches[$matches.Count - 1] # Get the last one which matches the top most file in the recursive matches
-            [xml]$vsixXml = Get-Content $vsixManifest
-
-            $ns = New-Object System.Xml.XmlNamespaceManager $vsixXml.NameTable
-            $ns.AddNamespace("ns", $vsixXml.DocumentElement.NamespaceURI) | Out-Null
-
-            $attrVersion = ""
-
-            if ($vsixXml.SelectSingleNode("//ns:Identity", $ns)){ # VS2012 format
-                $attrVersion = $vsixXml.SelectSingleNode("//ns:Identity", $ns).Attributes["Version"]
-            }
-            elseif ($vsixXml.SelectSingleNode("//ns:Version", $ns)){ # VS2010 format
-                $attrVersion = $vsixXml.SelectSingleNode("//ns:Version", $ns)
-            }
-
-            $attrVersion.InnerText = $buildNumber
-
-            $vsixXml.Save($vsixManifest) | Out-Null
-
-            $buildNumber | Write-Host -ForegroundColor Green
-        }
+function UpdateVersion {
+    $manifestFilePath = Get-VstsInput -Name "FileName";
+    if ([string]::IsNullOrEmpty($manifestFilePath)) 
+    {
+        $manifestFilePath = ".\source.extension.vsixmanifest";
     }
+
+    $buildNumber = Get-VstsInput -Name "VersionNumber";
+    if ([string]::IsNullOrEmpty($buildNumber)) 
+    {
+        $buildNumber = $env:BUILD_BUILDNUMBER;
+    }
+
+    "Incrementing VSIX version..." | Write-Host  -ForegroundColor Cyan -NoNewline
+    
+    $matches = (Get-ChildItem $manifestFilePath -Recurse)
+    $vsixManifest = $matches[$matches.Count - 1] # Get the last one which matches the top most file in the recursive matches
+    [xml]$vsixXml = Get-Content $vsixManifest
+
+    $ns = New-Object System.Xml.XmlNamespaceManager $vsixXml.NameTable
+    $ns.AddNamespace("ns", $vsixXml.DocumentElement.NamespaceURI) | Out-Null
+
+    $attrVersion = ""
+
+    if ($vsixXml.SelectSingleNode("//ns:Identity", $ns)){ # VS2012 format
+        $attrVersion = $vsixXml.SelectSingleNode("//ns:Identity", $ns).Attributes["Version"]
+    }
+    elseif ($vsixXml.SelectSingleNode("//ns:Version", $ns)){ # VS2010 format
+        $attrVersion = $vsixXml.SelectSingleNode("//ns:Version", $ns)
+    }
+
+    $attrVersion.InnerText = $buildNumber
+
+    $vsixXml.Save($vsixManifest) | Out-Null
+
+    $buildNumber | Write-Host -ForegroundColor Green
 }
 
 Function Main() {
 
-    Vsix-IncrementVsixVersion $FileName $VersionNumber
+    UpdateVersion
 
 }
 
